90dbaad0ae44dd043a85d19c6c73f7fb1046bebf
Azamat <azaege@gmail.com>
2024-10-25 17:17:35 -0700

    add login component

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+export interface ILoginProps {
+}
+
+
+const Login: React.FunctionComponent<ILoginProps> = () => {
+    return (
+        <div>
+            Login
+        </div>
+    );
+  
+}
+
+export default Login
