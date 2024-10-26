0eacb08cb32d823e86b5e1de3480ea6bd3f7f643
Azamat <azaege@gmail.com>
2024-10-25 17:17:21 -0700

    add signup component

---
+++ src/pages/signup/index.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+export interface ISignupProps {
+}
+
+
+const Signup: React.FunctionComponent<ISignupProps> = () => {
+    return (
+        <div>
+            SignUp
+        </div>
+    );
+  
+}
+
+export default Signup
