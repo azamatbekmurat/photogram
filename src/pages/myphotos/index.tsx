6f70c7cc82e741e498ed095a811694fcfa1a3902
Azamat <azaege@gmail.com>
2024-10-27 16:06:21 -0700

    add MyPhotos component

---
+++ src/pages/myphotos/index.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+export interface IMyPhotosProps {
+}
+
+
+const MyPhotos: React.FunctionComponent<IMyPhotosProps> = () => {
+    return (
+        <div>
+            MyPhotos
+        </div>
+    );
+  
+}
+
+export default MyPhotos
