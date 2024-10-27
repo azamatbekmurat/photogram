6a50ff35fe730636336a10cac90bcb8190ecaa46
Azamat <azaege@gmail.com>
2024-10-27 16:06:37 -0700

    add Post component

---
+++ src/pages/post/index.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+export interface ICreatePostProps {
+}
+
+
+const CreatePost: React.FunctionComponent<ICreatePostProps> = () => {
+    return (
+        <div>
+            CreatePost
+        </div>
+    );
+  
+}
+
+export default CreatePost
