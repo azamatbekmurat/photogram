d321eca62c85a10dd6c94fa40250d5fc46d8b134
Azamat <azaege@gmail.com>
2025-03-06 12:30:59 -0800

    Update ProtectedRoutes

--- src/components/ProtectedRoutes/index.tsx
+++ src/components/ProtectedRoutes/index.tsx
@@ -1,14 +1,21 @@
+import { getAuth } from 'firebase/auth';
 import * as React from 'react';
 import { Navigate, Outlet, useLocation } from 'react-router-dom';
+import { useAuthState } from "react-firebase-hooks/auth"
 
 interface IProtectedRoutesProps {
 }
 
-const ProtectedRoutes: React.FunctionComponent<IProtectedRoutesProps> = (props) => {
-    const isAuth: boolean = true;
+const ProtectedRoutes: React.FunctionComponent<IProtectedRoutesProps> = () => {
+    const auth = getAuth();
+    const [user, loading] = useAuthState(auth);
     const location = useLocation();
 
-    return isAuth ? (<Outlet />) : (
+    if(loading) {
+        return <div>...Loading</div>
+    }
+
+    return user ? (<Outlet />) : (
         <Navigate to="/login" state={{ from: location }} />
     )
 };
