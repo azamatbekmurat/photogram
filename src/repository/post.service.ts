141fcfe97a26ae9f8c4683f9532200b222efcfd2
Azamat <azaege@gmail.com>
2025-03-22 14:19:35 -0700

    add updateLikesOnPost function

--- src/repository/post.service.ts
+++ src/repository/post.service.ts
@@ -1,5 +1,5 @@
 import { db } from "@/firebaseConfig";
-import { Post } from "@/types";
+import { DocumentResponse, Post } from "@/types";
 import {
   addDoc,
   collection,
@@ -10,6 +10,7 @@ import {
   orderBy,
   query,
   where,
+  updateDoc
 } from "firebase/firestore";
 
 const COLLECTION_NAME = "posts";
@@ -18,9 +19,33 @@ export const createPost = (post: Post) => {
   return addDoc(collection(db, COLLECTION_NAME), post);
 };
 
-export const getPosts = () => {
-  const q = query(collection(db, COLLECTION_NAME), orderBy("date", "desc"));
-  return getDocs(q);
+export const getPosts = async () => {
+  try {
+    const q = query(collection(db, COLLECTION_NAME), orderBy("date", "desc"));
+    const querySnapshot = await getDocs(q);
+    const tempArr: DocumentResponse[] = [];
+
+    if(querySnapshot.size > 0) {
+      querySnapshot.forEach((doc) => {
+        const data = doc.data() as Post;
+        const responseObj: DocumentResponse = {
+          id: doc.id,
+          ...data
+        }
+
+        tempArr.push(responseObj);
+      });
+
+      return tempArr;
+    }
+    else {
+      console.log("No such document");
+    }
+  }
+  catch(error) {
+    console.log(error);
+  }
+  
 };
 
 export const getPostByUserId = (id: string) => {
@@ -35,4 +60,12 @@ export const getPost = (id: string) => {
 
 export const deletePost = (id: string) => {
   return deleteDoc(doc(db, COLLECTION_NAME, id));
+};
+
+export const updateLikesOnPost = (id: string, userlikes: string[], likes: number) => {
+  const docRef = doc(db, COLLECTION_NAME, id);
+  return updateDoc(docRef, {
+    likes,
+    userlikes
+  });
 };
